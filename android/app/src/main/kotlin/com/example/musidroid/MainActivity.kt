80bfecf721ba3b28e67091b32b96a77d8d4b1110
novincdavis123 <novindavisks@gmail.com>
2023-03-25 12:01:47 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/musidroid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.musidroid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flame_splash_screen/flame_splash_screen.dart';
+import 'package:musidroid/screens/Musihome.dart';
+
+void main() {
+  runApp(MaterialApp(useInheritedMediaQuery: true,
+    theme: ThemeData(primarySwatch: Colors.blue),
+    debugShowCheckedModeBanner: false,
+    home: MusiSplash(),
+  ));
+}
+class MusiSplash extends StatelessWidget {
+  const MusiSplash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlameSplashScreen(
+  theme: FlameSplashTheme.dark,
+  showBefore: (BuildContext context) {
+    return Text("To be shown before flame animation",style: TextStyle(color: Colors.amber),);
+  },
+  onFinish: (BuildContext context) =>  Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => Musify()))
+)
+    );
+  }
+}
